96ad434ca6638c314824eb72e0fdac0b690a377b
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-02-28 16:24:22 +0300

    Создание action  и  mutation  для регистрации пользователя

--- src/store/user.js
+++ src/store/user.js
@@ -1,12 +1,33 @@
+class User {
+    constructor(id, email, password) {
+        this.id = id
+        this.email = email
+        this.password = password
+    }
+}
+
 export default {
 	state: {
-        user:null
+        user: null
     },
-	mutations: {},
-	actions: {},
+	mutations: {
+        setUser(state,payload) {
+            console.log(payload)
+            state.user = payload
+        }
+    },
+
+	actions: {
+        registerUser({commit},{email, password}){
+            //Здесь запрос на сервер для регистрации
+            commit('setUser', new User(1, email, 	password))
+        }
+    },
+
 	getters: {
-        user(state){
+        user(state) {
             return state.user
         }
     }
 }
+
