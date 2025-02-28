9cc93e1adcd57b5578de7cfb8be7616e237afe15
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-02-28 15:05:05 +0300

    Просмотр деталей объявления

--- src/store/ads.js
+++ src/store/ads.js
@@ -42,6 +42,11 @@ export default {
       },
       myAds(state) {
         return state.ads;
+      },
+      adByld(state) {
+        return id => {
+          return state.ads.find(ad => ad.id == id);
+        };
       }
     }
   };
