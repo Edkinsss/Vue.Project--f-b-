936f86765237cc7461b3771f0a2bcbef98a1e1b4
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-03-01 22:31:20 +0300

    Логика отображений объявлений пользователя

--- src/store/ads.js
+++ src/store/ads.js
@@ -1,84 +1,90 @@
 export default {
-    state: {
-      ads: [
+  state: {
+      ads:[
         {
-          title: "First",
-          desc: "First Desc",
+          title:"First",
+          desc:"First Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-          id: "1",
+          id:"1",
           userId: "1"
         },
         {
-          title: "Second",
-          desc: "Second Desc",
+          title:"Second",
+          desc:"Second Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-          id: "2"
+          id:"2",
+          userId: "1"
         },
         {
-          title: "Third",
-          desc: "Third Desc",
+          title:"Third",
+          desc:"Thitd Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-          id: "3"
+          id:"3",
+          userId: "1"
         },
         {
-          title: "Fourth",
-          desc: "Fourth Desc",
+          title:"Fouth",
+          desc:"Fouth Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-          id: "4"
+          id:"4",
+          userId: "1"
         }
-      ]
-    },
-    mutations: {
-        createAd(state, payload){
-            state.ads.push(payload)
-        }
-    },
-    actions: {
-        async createAd({commit, getters},payload){
-            payload.id = Math.random()
-            payload.userId = getters.user != null ? getters.user.id :'1'
-            commit('clearError')
-            commit('setLoading', true)
-            //Заглушка запроса
-            let isRequestOk = true
-            let promise = new Promise(function(resolve) {
-              setTimeout(() => resolve('Done')
-              , 3000);
-            });
-            if (isRequestOk) {
-              await promise.then(()=> {
-                commit('createAd', payload)
-                commit('setLoading', false)
-              })
-            } else {
-              await promise.then(()=> {
-                commit('setLoading', false)
-                commit('setError', 'Ошибка создания объявления')
-                throw 'Упс... Ошибка создания объявления'
-              })
-            }
-        }
-    },
-    getters: {
+    ]
+  },
+  mutations: {
+      createAd(state, payload){
+      state.ads.push(payload)
+    }
+  },
+  actions: {
+      async createAd({commit, getters},payload){
+  payload.id = Math.random()
+  payload.userId = getters.user != null ? getters.user.id : '1'
+  commit('clearError')
+  commit('setLoading', true)
+  //Заглушка запроса
+  let isRequestOk = true
+  let promise = new Promise(function(resolve) {
+  setTimeout(() => resolve('Done')
+  , 3000);
+  });
+  if (isRequestOk) {
+  await promise.then(()=> {
+  commit('createAd', payload)
+  commit('setLoading', false)
+  })
+  } else {
+  await promise.then(()=> {
+  commit('setLoading', false)
+  commit('setError', 'Ошибка создания объявления')
+  throw 'Упс... Ошибка создания объявления'
+  })
+  }
+    }
+  
+  },
+  getters: {
       ads(state) {
-        return state.ads;
+        return state.ads
       },
       promoAds(state) {
         return state.ads.filter(ad => {
-            return ad.promo
+          return ad.promo
         })
       },
-      myAds(state) {
-        return state.ads;
-      },
-      adByld(state) {
-        return id => {
-          return state.ads.find(ad => ad.id == id);
-        };
-      }
+      myAds(state, getters) {
+  return state.ads.filter(ad => {
+  return ad.userId == getters.user.id
+  })
+  },
+      adById(state) {
+    return id => {
+    return state.ads.find(ad => ad.id == id)
     }
-  };
+  }
+  }
+  }
